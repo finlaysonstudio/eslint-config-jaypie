9d3e0cba9cc3137ba1c39a9704ba24b947bdb8df
Adam Finlayson <amf@adamfinlayson.com>
2024-07-22 10:54:20 -0500

    feat: init: flat config style

---
+++ flat.spec.js
@@ -0,0 +1,25 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+// Subject
+import flat from "./flat.js";
+
+//
+//
+// Mock modules
+//
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+//
+//
+// Run tests
+//
+
+describe("Flat", () => {
+  it("Works", () => {
+    expect(flat).not.toBeUndefined();
+    expect(Array.isArray(flat)).toBe(true);
+  });
+});
